4992e84b652ba57657f610e823f14014ff354a37
Sadaan Khan Durrani <sadaankhan2346@gmail.com>
2025-09-19 22:36:42 +0500

    Exercise 14 from the youtube video of CodeWithHarry Web Development Playlist

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 let arr_1 = [1,2,3,4,5,6, 54, 75];
 let arr_2 = ["Sadaan", "Harris", "Ahmed", "Samad"];
 
-// console.log(arr_2);
+console.log(arr_2);
 
 // // -- Array Methods --
 
@@ -13,92 +13,92 @@ console.log(arr_2.toString());
 
 // // join()
 
-// console.log(arr_2.join(" ; ")); // ',' ki jagah ab space, semicolon, space use hoga.
+console.log(arr_2.join(" ; ")); // ',' ki jagah ab space, semicolon, space use hoga.
 
 // // pop
 
-// let yo = arr_1.pop();
-// console.log(arr_1);
+let yo = arr_1.pop();
+console.log(arr_1);
 
 // //push
 
-// let yo_2 = arr_1.push(6, 87);
-// console.log(arr_1);
+let yo_2 = arr_1.push(6, 87);
+console.log(arr_1);
 
 // //shift
 
-// let yo_1 = arr_1.shift();
-// console.log(arr_1);
+let yo_1 = arr_1.shift();
+console.log(arr_1);
 
 // //unshift
 
-// let yo_3 = arr_1.unshift(12);
-// console.log(arr_1);
+let yo_3 = arr_1.unshift(12);
+console.log(arr_1);
 
 // //delete
 
-// let yo_4 = delete arr_1[5];
-// console.log(arr_1);
+let yo_4 = delete arr_1[5];
+console.log(arr_1);
 
 // //concat
 
-// let a1 = [1,2,3];
-// let a2 = [4,5,6];
-// let a3 = [7,8,9];
+let a1 = [1,2,3];
+let a2 = [4,5,6];
+let a3 = [7,8,9];
 
-// let yo_5 = a1.concat(a2, a3);
-// console.log(yo_5);
+let yo_5 = a1.concat(a2, a3);
+console.log(yo_5);
 
-// //Do not change the existing arrays. a1, a2 and a3 will remain the same.
+//Do not change the existing arrays. a1, a2 and a3 will remain the same.
 
-// // sort
+// sort
 
-// let a4 = [6, 7, 11,2,12,54,224];
-// a4.sort();
-// console.log(a4);
+let a4 = [6, 7, 11,2,12,54,224];
+a4.sort();
+console.log(a4);
 
 // //splice
 
-// let arr = [12,21,34,54,34,656];
-// //                       start  deleteCount  Value to add in that place
-// let newArray = arr.splice( 0,         3,             122);
-// console.log(arr);
+let arr = [12,21,34,54,34,656];
+//                       start  deleteCount  Value to add in that place
+let newArray = arr.splice( 0,         3,             122);
+console.log(arr);
 
 // //slice
 
-// let a6 = [111,222,333,444];
-// console.log(a6.slice(0, 2)); // 0 se start karo aur 1 tak jao, Not including 1.
+let a6 = [111,222,333,444];
+console.log(a6.slice(0, 2)); // 0 se start karo aur 1 tak jao, Not including 1.
 
 // //reverse
 
-// let a7 = [9,8,7,6,5,4,3,2,1];
-// console.log(a7.reverse());
+let a7 = [9,8,7,6,5,4,3,2,1];
+console.log(a7.reverse());
 
 // Array.from()
 
-// let name = "Sadaan Khan";
-// console.log(Array.from(name));
+let name = "Sadaan Khan";
+console.log(Array.from(name));
 
 // ----------------------------------------
 
 // -- Loops and Arrays --
 
-// let array_ = [1,43,54,23,2,12];
-// let i;
+let array_ = [1,43,54,23,2,12];
+let i;
 
-// for(i = 0; i < array_.length; i++){
+for(i = 0; i < array_.length; i++){
 
-//     console.log(array_[i]);
+    console.log(array_[i]);
 
-// }
+}
 
 // printing array using for of loop
 
-// for(const value of array_){
+for(const value of array_){
 
-//     console.log(value);
+    console.log(value);
 
-// }
+}
 
 // --------
 
@@ -106,19 +106,19 @@ console.log(arr_2.toString());
 
 // Note: Isski madad se hum maujooda array per aesa operations perform karasakhte hain jo uske har element per apply honge, Aur usse aik naye array mein save karlenge.
 
-// let array__ = [1,3,5,7,9];
-// let changed_array__ = array__.map((e) =>{
+let array__ = [1,3,5,7,9];
+let changed_array__ = array__.map((e) =>{
 
-//     // 1. .map() array mein maujood har element ko aik aik karke process karta hain.
-//     // 2. 'e' currently process hone waale element ko represent karta hain.
-//     //3. Usske baad aik arrow function lagaya jaa raha hain.
+    // 1. .map() array mein maujood har element ko aik aik karke process karta hain.
+    // 2. 'e' currently process hone waale element ko represent karta hain.
+    //3. Usske baad aik arrow function lagaya jaa raha hain.
 
-//     return e**2;
+    return e**2;
 
 
-// });
+});
 
-// console.log(changed_array__);
+console.log(changed_array__);
 
 // ---------
 
@@ -141,30 +141,30 @@ console.log(ar.filter(greterThanTen));
 
 //or 
 
-// const greterThanTen = ar.filter((e) =>{
+const greterThanTen = ar.filter((e) =>{
 
-//     if(e > 10){
+    if(e > 10){
     
-//         return true;
+        return true;
 
-//     }
-//     return false;
+    }
+    return false;
 
-// })
+})
 
-// console.log(greterThanTen);
+console.log(greterThanTen);
 
 // -------
 
 // reduce
 
-// let cr = [1,2,3,4,5,6];
+let cr = [1,2,3,4,5,6];
 
-// const multiplication = (a,b) =>{
+const multiplication = (a,b) =>{
     
-//     return a*b;
+    return a*b;
 
-// }
+}
 
 // Ye pehle do elements lega, Such as '1' aur '2', Aur phir inn dono ko multiply karega, Aur jo answer aayega usse '3' se multiply karega, Phir jo answer aayega usse '4' se multiply karega and so on...
 
